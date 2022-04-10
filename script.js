816c5deee9966647a26adc72b849b9024a0c70bf
rafaelsoares07 <rafaelsoares1936@gmail.com>
2022-04-09 21:12:32 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const url = "https://rickandmortyapi.com/api/character/"
+let numPerson = prompt('Digite um número de 0 a 600 e em seguida clique em buscar para descobrir quem você seria em Rick And Morty')
+
+
+
+//Pegar dados da API
+function buscarDados(){
+    axios.get(url+numPerson)
+    .then(response=>{
+        let data = response.data
+
+        rederizarPersonagem(data)
+
+    })
+    .catch(error => console.log(error))
+    
+}
+
+function rederizarPersonagem(personagem){
+    document.querySelector(".imagem-do-personagem").src = personagem.image
+    document.querySelector(".nome").innerHTML = personagem.name
+    document.querySelector(".especie").innerHTML = personagem.species
+
+}
+
+
+buscarDados()
+
